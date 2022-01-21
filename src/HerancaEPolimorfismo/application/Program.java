198e3ee1f60e41bdcf010c0ee572b62a9f9afea1
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-01-21 13:46:44 -0300

    Seção 14 Herança e Polimorfismo - Aula 125

--- src/HerancaEPolimorfismo/application/Program.java
+++ src/HerancaEPolimorfismo/application/Program.java
@@ -1,4 +1,15 @@
 package HerancaEPolimorfismo.application;
 
+import HerancaEPolimorfismo.entities.BusinessAccount;
+
 public class Program {
+    public static void main(String[] args) {
+
+        BusinessAccount businessAccount = new BusinessAccount();
+
+
+
+    }
+
+
 }
--- src/HerancaEPolimorfismo/entities/Account.java
+++ src/HerancaEPolimorfismo/entities/Account.java
@@ -1,4 +1,50 @@
 package HerancaEPolimorfismo.entities;
 
 public class Account {
+
+    private Integer number;
+    private String holder;
+    protected Double balance;
+
+    //-----------------------------------Construtor--------------------------------------------
+    public Account(){
+    }
+
+    public Account(Integer number, String holder, Double balance) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    //---------------------------------Getters e Setters----------------------------------------
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    //-------------------------------------Métodos----------------------------------------------
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
 }
