0b8881a3095147cfce2bea962f1142281e704932
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-01-21 14:36:53 -0300

    Seção 14 Herança e Polimorfismo - Aula 126 Upcasting e Downcasting

--- src/HerancaEPolimorfismo/application/Program.java
+++ src/HerancaEPolimorfismo/application/Program.java
@@ -1,15 +1,42 @@
 package HerancaEPolimorfismo.application;
 
+import HerancaEPolimorfismo.entities.Account;
 import HerancaEPolimorfismo.entities.BusinessAccount;
+import HerancaEPolimorfismo.entities.SavingsAccount;
 
 public class Program {
     public static void main(String[] args) {
 
-        BusinessAccount businessAccount = new BusinessAccount();
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria",
+                0.0, 500.00);
 
+        //------------------------------UPCASTING---------------------------------------------
 
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob",
+                0.0, 200.00);
+        Account acc3 = new SavingsAccount(1004, "Anna",
+                0.0, 0.01);
+
+        //-----------------------------DOWNCASTING---------------------------------------------
+
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100.00);
+
+        // BusinessAccount acc5 = (BusinessAccount)acc3; -> Error! Erro nas classes
+
+        if (acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.loan(200.00);
+            System.out.println("Loan!");
+        }
+
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
 
     }
-
-
 }
---
+++ src/HerancaEPolimorfismo/entities/SavingsAccount.java
@@ -0,0 +1,34 @@
+package HerancaEPolimorfismo.entities;
+
+public class SavingsAccount extends Account {
+
+    private Double interestRate;
+
+    //-----------------------------------Construtor--------------------------------------------
+
+    public SavingsAccount(){
+        super();
+    }
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interestrate) {
+        super(number, holder, balance);
+        this.interestRate = interestrate;
+    }
+
+    //---------------------------------Getters e Setters----------------------------------------
+
+    public Double getInterestrate() {
+        return interestRate;
+    }
+
+    public void setInterestrate(Double interestrate) {
+        this.interestRate = interestrate;
+    }
+
+    //-------------------------------------Métodos----------------------------------------------
+
+    public void updateBalance(){
+        balance += balance * interestRate;
+    }
+
+}
