850a8b3ec0c36b663be2105200ace45cbcd5566b
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-01-24 13:25:04 -0300

    Seção 14: Herança e Polimorfismo - Aula 129 Introdução ao Polimorfismo

---
+++ src/HerancaEPolimorfismo/application/ProgramPolimorfismo.java
@@ -0,0 +1,22 @@
+package HerancaEPolimorfismo.application;
+
+import HerancaEPolimorfismo.entities.Account;
+import HerancaEPolimorfismo.entities.SavingsAccount;
+
+public class ProgramPolimorfismo {
+    public static void main(String[] args) {
+
+        Account x = new Account(1020, "Alex", 1000.00);
+        Account y = new SavingsAccount(1023, "Maria",
+                1000.00, 0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
+
+
+
+    }
+}
