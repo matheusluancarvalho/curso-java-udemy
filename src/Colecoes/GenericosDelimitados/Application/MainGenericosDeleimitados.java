2df4566407f21911df20254b61a5c12c9e1bcd06
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-02-02 15:35:09 -0300

    Seção 19: Genéricos, Conjunto, Mapa - Aula 206 Genéricos Delimitados

---
+++ src/Colecoes/GenericosDelimitados/Application/MainGenericosDeleimitados.java
@@ -0,0 +1,40 @@
+package Colecoes.GenericosDelimitados.Application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import Colecoes.GenericosDelimitados.Entities.Product;
+import Colecoes.GenericosDelimitados.Services.CalculationService;
+
+public class MainGenericosDeleimitados {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        String path = "C:\\temp\\in.txt"; // Redirecione o diretório
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.println("Most expensive:");
+            System.out.println(x);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
