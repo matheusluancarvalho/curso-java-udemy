690643916c2129b32046254dd1d5da905bb7b2c8
Matheus Carvalho <matheusluancarvalho@gmail.com>
2022-02-16 14:58:41 -0300

    Seção 19: Generics, Set, Map - Aula 211 Como set testa igualdade

---
+++ src/Colecoes/Set/ComoSetTestaIgualdade/Program/Main.java
@@ -0,0 +1,20 @@
+package Colecoes.Set.ComoSetTestaIgualdade.Program;
+
+import Colecoes.Set.ComoSetTestaIgualdade.Entities.Product;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product("Notebook", 1200.0); // Avalia o conteudo e não a referẽncia do ponteiro
+
+        System.out.println(set.contains(prod));
+    }
+}
